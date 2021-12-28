5afd5142a0738f6f163b2b7d232bb8e30fa9c5a3
Yash Dhanore <yashdhanore@gmail.com>
2021-12-28 23:22:59 +0100

    Imports native vector icons

--- src/screens/CityScreen.tsx
+++ src/screens/CityScreen.tsx
@@ -1,7 +1,8 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, Button, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Styles } from "../library/Styles";
 import React from "react";
+import {} from "react-native-vector-icons";
 
 const SearchBox = () => {
   return (
