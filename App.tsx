1687c72a1ddd2e6572163fcbc5f32eef31032666
Yash Dhanore <yashdhanore@gmail.com>
2021-12-28 14:37:59 +0100

    Adds buttons and screens for specific buttons

--- App.tsx
+++ App.tsx
@@ -1,14 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button,StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native'
 import {createStackNavigator} from '@react-navigation/stack'
+import * as React from 'react'
 
 const Stack = createStackNavigator();
+const StackNavigator = (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Home"
+      component={HomeScreen} // <----
+    />
+    <Stack.Screen
+      name="Details"
+      component={CityScreen} // <----
+    />
+  </Stack.Navigator>
+);
 
-function HomeScreen() {
+function HomeScreen({navigation}: {navigation: any}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Home Screen</Text>
+      <Button
+        title="Search By City"
+        onPress={() => navigation.navigate('Search By City')}
+      />
+      <Button
+        title="Search By Country"
+        onPress={() => navigation.navigate('Search By Country')}
+      />
+    </View>
+  );
+}
+
+function CityScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Seacrhed by City</Text>
+    </View>
+  );
+}
+
+function CountryScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Seacrhed by Country</Text>
     </View>
   );
 }
@@ -17,7 +54,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="CityPop" component={HomeScreen} />
+        <Stack.Screen name="CityPop" component={HomeScreen} options={{ title: 'CityPop' }}/>
+        <Stack.Screen name="Search By City" component={CityScreen} options={{ title: 'Search By City' }}/>
+        <Stack.Screen name="Search By Country" component={CountryScreen} options={{ title: 'Search By Country'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
